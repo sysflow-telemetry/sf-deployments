ab0e5b651fc171ffff4980125bca9a6fadf3f325
Teryl <terylt@ibm.com>
2019-10-29 19:23:44 +0000

    removed references to COS and internal githubs.  removed analytics helm and test harness

--- helm/test-harness/load-test/modules/attack/attacks/api-injection.js
+++
@@ -1,4 +0,0 @@
-global.app.get('/malicious', function(rq,re) {
-    re.send("This is an injected malicious API endpoint talking to you!");
-})
-console.log('API injected!');
--- helm/test-harness/load-test/modules/attack/attacks/command.js
+++
@@ -1,1 +0,0 @@
-require('child_process').exec('COMMAND', function(error, stdout, stderr) { console.log(stdout) });
