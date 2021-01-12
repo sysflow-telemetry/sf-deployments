4855016f7f82990e397696fa6edc94c1d3715b08
Frederico Araujo <araujof@gmail.com>
2021-01-12 17:57:47 +0000

    fixed deployment of processor chart
    Signed-off-by: Frederico Araujo <araujof@gmail.com>

--- helm/scripts/installExporterChart.sh
+++ helm/scripts/installExporterChart.sh
@@ -19,7 +19,7 @@
 # limitations under the License.
 
 
-if [ "$#" -ne 4 ]; then
+if [ "$#" -ne 5 ]; then
     echo "Required arguments missing!"
     echo "Usage : ./installExporterChart <s3_region> <s3_access_key> <s3_secret_key> <s3_endpoint>"
     echo "<s3_region> value is the region in the S3 compliant object store (e.g., us-south)"
@@ -69,6 +69,6 @@ fi
 
 # sf-deployments/helm/scripts/
 REALPATH=$(dirname $(realpath $0))
-cd $REALPATH/charts
+cd $REALPATH/../charts
 
-helm install sf-exporter-chart -f sf-exporter-chart/values.yaml --namespace sysflow --set sfexporter.s3AccessKey=$s3AccessKey --set sfexporter.s3SecretKey=$s3SecretKey --set sfexporter.s3Endpoint=$s3Endpoint --set sfexporter.s3Location=$s3Region --set sfexporter.s3Bucket=$s3Bucket --debug ./sf-exporter-chart
+helm install sysflowagent ./sf-exporter-chart -f sf-exporter-chart/values.yaml --namespace sysflow --set sfexporter.s3AccessKey=$s3AccessKey --set sfexporter.s3SecretKey=$s3SecretKey --set sfexporter.s3Endpoint=$s3Endpoint --set sfexporter.s3Location=$s3Region --set sfexporter.s3Bucket=$s3Bucket --debug
--- helm/scripts/installProcessorChart.sh
+++ helm/scripts/installProcessorChart.sh
@@ -46,6 +46,6 @@ fi
 
 # sf-deployments/helm/scripts/
 REALPATH=$(dirname $(realpath $0))
-cd $REALPATH/charts
+cd $REALPATH/../charts
 
-helm install sf-processor-chart -f sf-processor-chart/values.yaml --namespace $NAMESPACE --set sfprocessor.export=syslog --set sfprocessor.syslogHost=$RSYSLOG_IP --set sfprocessor.syslogPort=$RSYSLOG_PORT --set sfprocessor.syslogProto=$RSYSLOG_PROTO --debug ./sf-processor-chart
+helm install sysflowagent ./sf-processor-chart -f sf-processor-chart/values.yaml --namespace $NAMESPACE --set sfprocessor.export=syslog --set sfprocessor.syslogHost=$RSYSLOG_IP --set sfprocessor.syslogPort=$RSYSLOG_PORT --set sfprocessor.syslogProto=$RSYSLOG_PROTO --debug
